25769e463821992daceecfd46d6559761b82a1bb
evgalp <evgeniyalpayev@gmail.com>
2018-03-22 16:13:00 +0200

    a basic modal

---
+++ modal/js/modal.js
@@ -0,0 +1,26 @@
+class Modal {
+  constructor(modalName) {
+    this.showBtn = document.querySelector(`[data-for-modal=\'${modalName}\']`)
+    this.modalWrapper = document.querySelector(`[data-modal=\'${modalName}\']`);
+    this.closeBtn = document.querySelector(`[data-modal=\'${modalName}\']`).children[0].children[0].children[1];
+
+    this.showBtn.addEventListener('click', this.showModal.bind(this));
+    this.closeBtn.addEventListener('click', this.hideModal.bind(this));
+  }
+
+  showModal() {
+    console.log(this.modalWrapper);
+    this.modalWrapper.classList.add('ui-modal-wrappper--active-modal');
+  }
+
+  hideModal() {
+    console.log(this.modalWrapper);
+    this.modalWrapper.classList.remove('ui-modal-wrappper--active-modal');
+  }
+
+
+
+}
+
+const m1 = new Modal('modal-1');
+const m2 = new Modal('modal-2');
