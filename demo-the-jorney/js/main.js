a41254b1cfdc1adf6a509d72873fcc8a2b0dcc38
evgalp <evgeniyalpayev@gmail.com>
2018-04-04 00:20:41 +0300

    gallery lightbox in thejorney & more

---
+++ demo-the-jorney/js/main.js
@@ -0,0 +1,23 @@
+class Gallery {
+  constructor(galleryId, lightboxId) {
+    this.gallery = document.getElementById(`${galleryId}`);
+    this.galleryLightbox = document.getElementById(`${lightboxId}`);
+
+
+    this.gallery.addEventListener('click', this.getCurrentImage.bind(this));
+
+    this.galleryLightbox.addEventListener('click', this.hideLightbox.bind(this));
+
+  }
+
+  getCurrentImage(e){
+    this.galleryLightbox.children[0].children[0].src = e.target.src;
+    this.galleryLightbox.style.display = "flex";
+  }
+
+  hideLightbox(e){
+    this.galleryLightbox.style.display = "none";
+  }
+}
+
+let g1 = new Gallery('gallery1', 'gallery-lightbox1');
