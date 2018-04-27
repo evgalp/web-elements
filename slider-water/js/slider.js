cd83c268370dd26c9edc2a695425b6f98221fe85
evgalp <evgeniyalpayev@gmail.com>
2018-04-27 16:26:33 +0300

    commit

--- slider-water/js/slider.js
+++
@@ -1,5 +0,0 @@
-const renderer = new PIXI.autoDetectRenderer();
-const stage = new PIXI.Container();
-const slidesContainer = new PIXI.Container();
-const displacementSprite = new PIXI.Sprite.fromImage( displacementImage );
-const displacementFilter = new PIXI.filters.DisplacementFilter( displacementImage )
